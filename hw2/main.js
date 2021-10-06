aa223a2327471760dcea1ef605e3ad389a1b305d
jasperchan5 <jscnn51011@gmail.com>
2021-10-06 16:58:12 +0800

    Add code HW2

--- hw2/main.js
+++ hw2/main.js
@@ -1,3 +1,19 @@
-function changeRightPage(){
-    rightArrow = document.getElementById("")
+var imgArr = ["starburst.jpg","uso.jpg","starburst2.jfif","ten.jfif"];
+var loadImage = function(id){
+    var imgBox = document.getElementById("large_img");
+    imgBox.src = imgArr[id];
+    
+    var container = document.getElementsByClassName("photo_section_2")
+    for(var i =0 ; i < imgArr.length ; i++){
+        if(i===id){
+            container[i].style.border = "#ffffff 10px dashed" 
+        }
+        else{
+            container[i].style.border = "#16151a 2px solid"
+        }
+    }
+    
+}
+var switchPage = function(){
+    alert("Null!!!")
 }
