2da06a436d60500fb4ce8265ca2d92446b8fdd50
jasperchan5 <jscnn51011@gmail.com>
2021-10-18 16:55:25 +0800

    Add code

--- hw3/main.js
+++ hw3/main.js
@@ -22,8 +22,7 @@ bottomList.id = "todo-footer";
 root.appendChild(bottomList);
 var totalNum = document.createElement("div");
 totalNum.classList += "todo-app__total";
-totalNum.innerText = "1 Left";
-
+totalNum.innerText = "-999 Left";
 var viewButtons = document.createElement("ul");
 viewButtons.classList += "todo-app__view-buttons";
 for(var i=0; i<3; i++){
@@ -43,23 +42,30 @@ clearButton.appendChild(button);
 bottomList.appendChild(totalNum);
 bottomList.appendChild(viewButtons);
 bottomList.appendChild(clearButton);
+bottomList.style.display = "none";
 
 // Add todo function
-var nowTodoCnt = 0;
+var nowTodoCnt = 0, displayTodoCnt = 0;
+var existTodo = false;
 var addTodo = function(itemName,nowTodoCnt){
     var newTodo = document.createElement("li");
     newTodo.classList += "todo-app__item";
     // Checkbox
     var checkBox = document.createElement("div");
     checkBox.classList += "todo-app__checkbox";
+    var checkInput = document.createElement("input");
+    checkInput.id = nowTodoCnt;
+    checkInput.type = "checkbox";
+    checkInput.onclick = function(){deleteLine(nowTodoCnt);};
     var confirm = document.createElement("label");
-    confirm.id = nowTodoCnt;
-    confirm.onclick = function(){highLight(nowTodoCnt)};
+    confirm.htmlFor = nowTodoCnt;
     nowTodoCnt += 1;
+    checkBox.appendChild(checkInput);
     checkBox.appendChild(confirm);
     // Item
     var item = document.createElement("h1");
     item.classList += "todo-app__item-detail";
+    item.style = "";
     item.innerText = itemName;
     // Delete button
     var deleteButton = document.createElement("img");
@@ -69,28 +75,42 @@ var addTodo = function(itemName,nowTodoCnt){
     newTodo.appendChild(item);
     newTodo.appendChild(deleteButton);
     listFrame.appendChild(newTodo);
+    // Display the footer
+    if(existTodo === false){
+        existTodo = true;
+        document.getElementsByTagName("footer")[0].style.display = "flex";
+    }
+    var displayTotal = document.getElementsByClassName("todo-app__total")[0];
+    displayTodoCnt = nowTodoCnt;
+    displayTotal.innerText = ""+ displayTodoCnt +" Left";
+    return nowTodoCnt;
 }
 
+// Add todo when enter key is pressed.
 document.addEventListener('keypress',function(e){
     if(e.key === 'Enter'){
         var itemName = document.getElementsByClassName("todo-app__input")[0].value;
         if(itemName != ""){
-            addTodo(itemName,nowTodoCnt);
+            nowTodoCnt =  addTodo(itemName,nowTodoCnt);
+            document.getElementsByClassName("todo-app__input")[0].value = "";
         }
     }
 })
 
-var highLight = function(id){
-    // if(!taskFinished[id]){
-    var item = document.getElementById(id);
-    console.log(item);
-    item.style.background = "#26ca299b;";
-    taskFinished[id] = true;
-    // }
-    // else{
-    //     var item = document.getElementById(id);
-    //     item.style.background = "rgba(99, 99, 99, 0.698);";
-    //     taskFinished[id] = false;
-    // }
+var deleteLine = function(todoCnt){
+    var checkBoxButton = document.getElementById(todoCnt-1);
+    var toDelete = document.getElementsByClassName("todo-app__item-detail")[todoCnt-1];
+    if(checkBoxButton.checked === true){
+        console.log("checked");
+        displayTodoCnt --;
+        document.getElementsByClassName("todo-app__total")[0].innerText = ""+ displayTodoCnt +" Left";
+        toDelete.style = "text-decoration: line-through; opacity: 0.5;";
+    }
+    else{
+        console.log("de-checked");
+        displayTodoCnt ++;
+        document.getElementsByClassName("todo-app__total")[0].innerText = ""+ displayTodoCnt +" Left";
+        toDelete.style = "";
+    }
     
 }
