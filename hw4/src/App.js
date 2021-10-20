a8a803c6ddefdec82a56204ec56a9b695e857721
jasperchan5 <jscnn51011@gmail.com>
2021-10-20 23:46:27 +0800

    Add code"

---
+++ hw4/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="root" class="todo-app__root">
+        <header className="App-header">
+          <h1 class="todo-app__title">todos</h1>
+          <script src="main.js"></script>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
