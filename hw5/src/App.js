645411ed7a0df8e7b3b3b3c134e987444cda54a3
jasperchan5 <jscnn51011@gmail.com>
2021-11-01 16:14:59 +0800

    Add code

--- hw5/src/App.js
+++ hw5/src/App.js
@@ -1,27 +1,68 @@
-import React, { Component } from 'react';
+import React, {Component,useState} from 'react';
 import './App.css';
 
-function App () {
-  const [nowNum, updateNum] = useState(0);
-  getNum = function(inputNum){
+// Refer to HTC calculator
+function RenderCalculator () {
+  const [nowNum, updateNum] = useState([""]);
+  const [result, updateResult] = useState();
+  const [status, updateStatus] = useState(false);
+  const turnOn_Off = () => { // Handle the turn on/off process
+    if(!status){
+      updateNum(["0"]);
+      updateResult(0);
+      updateStatus(true);
+    }
+    else{
+      updateNum([""]);
+      updateResult();
+      updateStatus(false);
+    }
+  };
+  let RenderPower = () => { // Change the power button
+    if(!status){
+      return <img src="power-off.png" style={{width: "30px",height: "30px"}}></img>;
+    }
+    else{
+      return <img src="power-on.png" style={{width: "30px",height: "30px"}}></img>
+    }
+  }
+  let SetResult = () => { //顯示目前預覽之運算結果
+    let result = nowNum;
+    return <div style={{textAlign: 'right',fontSize: '0.5cm',color: 'rgba(255,255,255,0.5)'}}>{result}</div>;
+  }
+  let ShowLatestResult = function(){ // 顯示最近一次運算結果在大數字上
     
   }
-  getOperator = function(operator){
-    
+  let clear = () => {
+    if(status){
+      updateNum(["0"]);
+      updateResult(0);
+    }
   }
-  show = function(){ // 顯示目前數字
+  let getNum = function(input){
+    if(status){
+      if(nowNum[0] === "0"){
+        updateNum([input]);
+      }
+      else{
+        updateNum([...nowNum,input]);
+      }
+    }
+  }
+  let getOperator = function(operator){
     
   }
   return (
     <div className="App">
       <div className="calculator__mainBody">
         <div className="calculator__displayWindow">
-          <div id="displayNum" style={{textAlign: 'right',fontSize: '1cm',color: 'rgba(255,255,255,0.8)'}}>{nowNum}</div>
+          <div style={{textAlign: 'right',fontSize: '1cm',color: 'rgba(255,255,255,0.8)'}}>{nowNum}</div>
+          <SetResult />
         </div>
         
         <div className="row">
-          <div className="offset-md-2 col-md-2">
-            <div className="calculator__button" onClick={()=>{this.getOperator('+');}}>+</div>
+          <div className="offset-md-1 col-md-2">
+            <div className="calculator__button" onClick={()=>{getOperator('+');}}>+</div>
           </div>
           <div className="col-md-2">
             <div className="calculator__button">-</div>
@@ -32,39 +73,42 @@ function App () {
           <div className="col-md-2">
             <div className="calculator__button">÷</div>
           </div>
+          <div className="col-md-2">
+            <div className="calculator__button" onClick={()=>{turnOn_Off()}}><RenderPower /></div>
+          </div>
         </div>
         
         <div className="row">
           <div className="offset-md-1-5 col-md-3">
-            <div className="calculator__button" onClick={() =>{this.getNum(7);}}>7</div>
+            <div className="calculator__button" onClick={() =>{getNum(7);}}>7</div>
           </div>
           <div className="col-md-3">
-            <div className="calculator__button" onClick={() => {this.getNum(8);}}>8</div>
+            <div className="calculator__button" onClick={() => {getNum(8);}}>8</div>
           </div>
           <div className="col-md-3">
-            <div className="calculator__button" onClick={() => {this.getNum(9);}}>9</div>
+            <div className="calculator__button" onClick={() => {getNum(9);}}>9</div>
           </div>
         </div>
         <div className="row">
           <div className="offset-md-1-5 col-md-3">
-            <div className="calculator__button" onClick={() =>{this.getNum(4);}}>4</div>
+            <div className="calculator__button" onClick={() =>{getNum(4);}}>4</div>
           </div>
           <div className="col-md-3">
-            <div className="calculator__button" onClick={() =>{this.getNum(5);}}>5</div>
+            <div className="calculator__button" onClick={() =>{getNum(5);}}>5</div>
           </div>
           <div className="col-md-3">
-            <div className="calculator__button" onClick={() =>{this.getNum(6);}}>6</div>
+            <div className="calculator__button" onClick={() =>{getNum(6);}}>6</div>
           </div>
         </div>
         <div className="row">
           <div className="offset-md-1-5 col-md-3">
-            <div className="calculator__button" onClick={() =>{this.getNum(1);}}>1</div>
+            <div className="calculator__button" onClick={() =>{getNum(1);}}>1</div>
           </div>
           <div className="col-md-3">
-            <div className="calculator__button" onClick={() =>{this.getNum(2);}}>2</div>
+            <div className="calculator__button" onClick={() =>{getNum(2);}}>2</div>
           </div>
           <div className="col-md-3">
-            <div className="calculator__button" onClick={() =>{this.getNum(3);}}>3</div>
+            <div className="calculator__button" onClick={() =>{getNum(3);}}>3</div>
           </div>
         </div>
         <br></br>
@@ -73,22 +117,13 @@ function App () {
             <div className="calculator__button">.</div>
           </div>
           <div className="col-md-2">
-            <div className="calculator__button" onClick={() => {this.getNum(0);}}>0</div>
+            <div className="calculator__button" onClick={() => {getNum(0);}}>0</div>
           </div>
           <div className="col-md-2">
-            <div className="calculator__button" onClick={() => {
-                this.state = {
-                  result:0, // 顯示在螢幕上的計算結果
-                  tempDisplay:0, // 運算過程中顯示在螢幕上的數字
-                  calcArr:Array(0), // 儲存所有輸入的數字、符號
-                  numArr:Array(0), // 儲存目前運算結果
-                  displayArr:Array(0) // 把calcArr裡的各字元組合並展示
-                }
-                document.getElementById("displayNum").innerText = this.state.result;
-            }}>C</div>
+            <div className="calculator__button" onClick={() => {clear()}}>C</div>
           </div>
           <div className="offset-md-1 col-md-2">
-            <div className="calculator__button" onClick={()=>{this.getOperator('=');this.calculate();}}>=</div>
+            <div className="calculator__button" onClick={()=>{getOperator('=');}}>=</div>
           </div>
         </div>
       </div>
@@ -96,4 +131,4 @@ function App () {
   );
 }
 
-export default App;
+export default RenderCalculator;
