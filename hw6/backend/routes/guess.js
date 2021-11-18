de565c6454c02f3e012f633d01985954348b266f
jasperchan5 <jscnn51011@gmail.com>
2021-11-18 15:31:03 +0800

    Add code

--- hw6/backend/routes/guess.js
+++ hw6/backend/routes/guess.js
@@ -11,21 +11,80 @@ router.post('/start',(_, res) =>{
     res.json({msg: 'The game has started.'})
 })
 router.get('/guess',(req,res) =>{
+    // Check if there is a string in guess
     const number = getNumber();
     const guessed = roughScale(req.query.number, 10);
-    console.log(guessed);
-    if(!guessed || guessed < 1 || guessed > 100){
+    var hasString = false;
+    for(let i=0; i<toString(guessed).length; i++){
+        if(parseInt(toString(guessed)[i]) === NaN){
+            hasString = true;
+            break;
+        }
+    }
+    let numArr = Array(2), guessArr = Array(2);
+    if(number < 10){
+        numArr[0] = number;
+    }
+    else{
+        numArr[0] = Math.floor(number/10);
+        numArr[1] = number%10;
+    }
+
+    if(guessed > 0 && guessed < 10){
+        guessArr[0] = guessed;
+    }
+    else if(guessed >= 10 && guessed < 100){
+        guessArr[0] = Math.floor(guessed/10);
+        guessArr[1] = guessed%10;
+    }
+
+    // // AB game
+    let A_cnt = 0, B_cnt = 0;
+    if(!guessed || guessed < 1 || guessed > 100 || hasString === true){
         res.status(406).send({msg:'Not a legal number'});
     }
-    else if(number === guessed){
-        res.status(200).send({msg:'Equal'});
+    else if(1 <= guessed && guessed < 10){
+        if(number === guessed){
+            res.status(200).send({msg:'Equal'});
+        }
+        else if((guessArr[0] === numArr[1]) && number !== guessed){
+            A_cnt++;
+        }
+        else if((guessArr[0] === numArr[0]) && number !== guessed){
+            B_cnt++;
+        }
+        res.status(200).send({msg: ''+ A_cnt +'A'+ B_cnt +'B'});
     }
-    else if(guessed > number){
-        res.status(200).send({msg:'Smaller, number is '+ number +''});
+    else if(10 <= guessed && guessed <= 100){
+        if(number === guessed){
+            res.status(200).send({msg:'Equal'});
+        }
+        else if((guessArr[0] === numArr[0] || guessArr[1] === numArr[1]) && number !== guessed){
+            A_cnt++;
+        }
+        else if((guessArr[1] === numArr[0] || guessArr[0] === numArr[1]) && number !== guessed){
+            B_cnt++;
+            if((guessArr[1] === numArr[0] && guessArr[0] === numArr[1])){
+                B_cnt++;
+            }
+        }
+        res.status(200).send({msg: ''+ A_cnt +'A'+ B_cnt +'B'});
     }
-    else if(guessed < number){
-        res.status(200).send({msg:'Bigger, number is '+ number +''});
-    }
+    
+
+    // Original code
+    // if(!guessed || guessed < 1 || guessed > 100){
+    //     res.status(406).send({msg:'Not a legal number'});
+    // }
+    // else if(number === guessed){
+    //     res.status(200).send({msg:'Equal'});
+    // }
+    // else if(guessed > number){
+    //     res.status(200).send({msg:'Smaller'});
+    // }
+    // else if(guessed < number){
+    //     res.status(200).send({msg:'Bigger'});
+    // }
 
 })
 router.post('/restart',(_, res) =>{
--- hw6/frontend/src/App.js
+++ hw6/frontend/src/App.js
@@ -6,24 +6,122 @@ function App() {
   const [hasStarted,setStart] = useState(false);
   const [hasWon,setHasWon] = useState(false);
   const [number,setNumber] = useState('');
+  const [comp_number,setComNumber] = useState('');
   const [status,setStatus] = useState('');
-  const handleGuess = async () =>{
+  const [comp_status,setComStatus] = useState('');
+  const [client,setClient] = useState(true);
+  const [compGuessArr] = useState([0,0,0,0,0,0,0,0,0,0]);
+  const [foundCorrectNum,setFoundCorrect] = useState(false);
+  const [correctNum,setCorrectNum] = useState(-1);
+  const [correctDigit,setCorrectDigit] = useState('');
+  const [guessButton,setGuessButton] = useState(false);
+
+  const setCompGuessArr = (number,status) => {
+    let ten = Math.floor(number/10), one = number%10;
+    if(status === '1A0B'){
+      if(compGuessArr[ten] === 1){ // Already A, so we can confirm it is A
+        setFoundCorrect(true);
+        setCorrectNum(ten);
+        setCorrectDigit('ten');
+      }
+      else if(compGuessArr[one] === 1){ // Already A, so we can confirm it is A
+        setFoundCorrect(true);
+        setCorrectNum(one);
+        setCorrectDigit('one');
+      }
+      else{
+        compGuessArr[ten] += 1;
+        compGuessArr[one] += 1;
+      }
+      console.log(compGuessArr);
+    }
+  }
+  const handleGuess = async () => {
     const response = await guess(number);
     if(response === 'Equal'){
       setHasWon(true);
       return;
     }
     else{
-      console.log("Set status ",response);
       setStatus(response);
-      setNumber('');
+      // Let the computer guess
+      // First set the client to false
+      if(client){
+        setClient(false);
+        // Guess a number according to current status
+        let randNum = 0;
+        if(comp_status === '0A0B' || comp_status === ''){
+         randNum = Math.floor(Math.random()*100); // Keep random guessing
+        }
+        else if(comp_status === '0A2B'){
+          randNum = comp_number%10*10 + Math.floor(comp_number/10); // Swap two digits
+        }
+        else if(comp_status === '1A0B'){
+          if(!foundCorrectNum){
+            setCompGuessArr(comp_number,comp_status);
+          }
+          console.log("Correct num: ",correctNum);
+          // Choose a digit as the correct one
+          if(correctNum === -1){
+            let randSelector = Math.random();
+            if(randSelector <= 0.5){ // Pick 10 digit
+              randNum = Math.floor(comp_number/10)*10 + Math.floor(Math.random()*10);
+            }
+            else{ // Pick 1 digit
+              randNum = comp_number%10 + Math.floor(Math.random()*10)*10;
+            }
+          }
+          else{
+            if(correctDigit === 'ten'){
+              randNum = correctNum*10 + Math.floor(Math.random()*10);
+            }
+            else if(correctDigit === 'one'){
+              randNum = correctNum + Math.floor(Math.random()*10)*10;
+            }
+            console.log("RandNum after determining the correct num: ",randNum);
+          }
+        }
+        else if(comp_status === '0A1B'){
+          // Choose a digit and add it into new guess
+          let randSelector = Math.random();
+          if(randSelector <= 0.25){ // Pick 10 digit, add to 10 digit
+            randNum = Math.floor(comp_number/10)*10 + Math.floor(Math.random()*10);
+          }
+          else if(randSelector > 0.25 && randSelector <= 0.5){ // Pick 10 digit, add to 1 digit
+            randNum = Math.floor(comp_number/10) + Math.floor(Math.random()*10)*10;
+          }
+          else if(randSelector > 0.5 && randSelector <= 0.75){ // Pick 1 digit, add to 10 digit
+            randNum = Math.floor(comp_number%10)*10 + Math.floor(Math.random()*10);
+          }
+          else{ // Pick 1 digit, add to 1 digit
+            randNum = comp_number%10 + Math.floor(Math.random()*10)*10;
+          }
+        }
+        // console.log(compGuessArr);
+        setComNumber(randNum);
+        const comp_response = await guess(randNum);
+        if(comp_response === 'Equal'){
+          setHasWon(true);
+          return;
+        }
+        else{
+          setComStatus(comp_response);
+          setClient(true);
+        }
+      }
     }
-    document.getElementsByClassName("guessGame__inputBar")[0].value = "";
+    document.getElementsByClassName("guessGame__inputBar")[0].value = '';
+    setGuessButton(false);
   }
  
   const startMenu = 
   <>
-    <div className="guessGame__title">Number guessing game</div>
+    <div className="guessGame__text title">Number guessing game</div>
+    <div className="guessGame__text rule">規則：</div>
+    <div className="guessGame__text rule">此遊戲經過改良，人與電腦會同時競爭猜同一個數字。</div>
+    <div className="guessGame__text rule">當你輸入數字並按下guess之後，電腦也會猜一個數字。</div>
+    <div className="guessGame__text rule">而status也不再是Bigger或Smaller，而是XAXB。</div>
+    <div className="guessGame__text rule">A表示數字跟位置都正確，B表示僅數字正確，位置不正確。</div>
     <button onClick={async ()=>{
       setStart(true);
       await startGame();
@@ -32,25 +130,32 @@ function App() {
 
   const gameMode = 
     <>
-    <div className="guessGame__title">Guess a number between 1 and 100</div>
+    <div className="guessGame__text title">Guess a number between 1 and 100</div>
       <div className="row">
-        <input className="guessGame__inputBar" onChange={(e)=>setNumber(e.target.value)}></input>
+        <input className="guessGame__inputBar" onChange={(e)=>{setNumber(e.target.value);setGuessButton(true)}}></input>
         <button className="guessGame__guessButton"
-                onClick={handleGuess}
-                disabled={!number}>Guess</button>
+                onClick = {handleGuess}
+                disabled={!guessButton}>Guess</button>
       </div>
-      <p className="guessGame__status">{status}</p>
+      <p className="guessGame__text status">You guess: {number}</p>
+      <p className="guessGame__text status">Your status: {status}</p>
+      <p className="guessGame__text status">Computer guess: {comp_number}</p>
+      <p className="guessGame__text status">Computer status: {comp_status}</p>
     </>
 
   const winningMode = 
     <>
-      <div className="guessGame__title">You won! The number was {number}.</div>
+      <div className="guessGame__text title">{client?"You won! The number was "+ number+ ".":"The computer won! The number was "+ comp_number +"."}</div>
       <button className="guessGame__restartButton" onClick={async ()=>{
         await restart();
         setHasWon(false);
         setStart(true);
         setNumber('');
+        setComNumber('');
         setStatus('');
+        setComStatus('');
+        setGuessButton(false);
+        setClient(true);
       }}>Restart</button>
     </>
   
